eb0800abee916041d7a06327cf560a69117d95d5
JoeyGrimsic <golfpro4034@gmail.com>
2026-01-02 16:39:21 -0500

    Updated with script

---
+++ cleanbuild.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+cmake -G "Unix Makefiles" -Bbuild/linux -DCMAKE_BUILD_TYPE=Release -DVKB_WSI_SELECTION=WAYLAND -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+cmake --build build/linux --config Release --target vulkan_samples -j$(nproc)
+./build/linux/app/bin/Release/x86_64/vulkan_samples sample hello_triangle
